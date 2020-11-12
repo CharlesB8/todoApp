f9b99a130a5557c2f639507d5796c2fd2343686f
Charles Bartels <charlieb2898@gmail.com>
2020-11-12 08:28:48 -0600

    setup

---
+++ src/main/java/com/example/todoApp/Config/PropConfig.java
@@ -0,0 +1,13 @@
+package com.example.todoApp.Config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+@PropertySource("/secrets.properties")
+@Configuration
+public class PropConfig {
+    @Autowired
+    Environment env;
+}
